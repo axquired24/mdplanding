a4b728b5f2fcf722eeae31abac8d2e6801f9f74c
Albert Septiawan <axquired24@gmail.com>
2017-12-06 11:28:01 +0000

    Apply fixes from StyleCI

--- config-mdp.php
+++ config-mdp.php
@@ -1,5 +1,5 @@
 <?php
-  $text   = [
+  $text = [
     // Lorem teks, isi dengan promo atau kata - kata menarik lainnya
     // Akhiri dengan koma, setelah petik
     'sub-text-atas'   => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt amet labore blanditiis repellat ratione consequuntur et, fugiat, rem, facere sit vel! Quas fugit accusamus, id sint, quasi facilis quam temporibus.',
@@ -11,8 +11,8 @@
     // Link sosial media
     // Akhiri dengan koma, setelah petik
     'instagram'   => 'https://www.instagram.com/mobilandigitalprinting/',
-    'facebook'   => '#',
-    'twitter'   => '#'
+    'facebook'    => '#',
+    'twitter'     => '#',
   ];
 
   $products = [
@@ -22,55 +22,55 @@
       'name'        => 'Mini Baner',
       'price'       => 'Rp 30.000',
       'description' => 'Satu set dengan kakinya',
-      'image_path'  => 'mini-banner.jpeg'
+      'image_path'  => 'mini-banner.jpeg',
     ],
     [
       'name'        => 'MMT',
       'price'       => 'Rp 18.000*',
       'description' => 'Standart - 280 gram. <br> *)per meter',
-      'image_path'  => 'mmt.jpeg'
+      'image_path'  => 'mmt.jpeg',
     ],
     [
       'name'        => 'Leaflet',
       'price'       => 'Rp 2.500*',
       'description' => 'Ukuran A4 (bolak balik) 150gr <br> *)per lembar',
-      'image_path'  => 'leaflet.jpeg'
+      'image_path'  => 'leaflet.jpeg',
     ],
     [
       'name'        => 'Kartu Nama <small>1 Muka</small>',
       'price'       => 'Rp 45.000',
       'description' => 'Isi 100 lembar (dapat kotaknya)',
-      'image_path'  => 'kartu-nama.jpeg'
+      'image_path'  => 'kartu-nama.jpeg',
     ],
     [
       'name'        => 'Kartu Nama <small>2 Muka</small>',
       'price'       => 'Rp 60.000',
       'description' => 'Isi 100 lembar (dapat kotaknya)',
-      'image_path'  => 'kartu-nama.jpeg'
+      'image_path'  => 'kartu-nama.jpeg',
     ],
     [
       'name'        => 'Art paper A3 <small>1 Muka</small>',
       'price'       => 'Rp 3.000',
       'description' => 'standart 150gr <br> *)per lembar',
-      'image_path'  => 'art-paper.jpeg'
+      'image_path'  => 'art-paper.jpeg',
     ],
     [
       'name'        => 'Art paper A3 <small>2 Muka</small>',
       'price'       => 'Rp 4.500',
       'description' => 'standart 150gr <br> *)per lembar',
-      'image_path'  => 'art-paper.jpeg'
+      'image_path'  => 'art-paper.jpeg',
     ],
     [
       'name'        => 'X Banner <small>340gr</small>',
       'price'       => 'Rp 80.000',
       'description' => 'Ukuran 60 x 160 cm (1 set dengan kakinya)',
-      'image_path'  => 'x-banner.jpeg'
+      'image_path'  => 'x-banner.jpeg',
     ],
     [
       'name'        => 'X Banner <small>280gr</small>',
       'price'       => 'Rp 65.000',
       'description' => 'Ukuran 60 x 160 cm (1 set dengan kakinya)',
-      'image_path'  => 'x-banner.jpeg'
+      'image_path'  => 'x-banner.jpeg',
     ],
     // Tambah produk baru disini dengan format:
     // [
@@ -80,5 +80,3 @@
     //   'image_path'  => 'x-banner.jpeg'
     // ],
   ]; // ! Jangan hapus penutup daftar produk ini
-
-?>
